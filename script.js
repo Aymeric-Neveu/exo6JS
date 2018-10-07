c6d688e3a7318f8f629f1b9402f72408e76fecb8
Aymeric Neveu <emploi.neveu@gmail.com>
2018-10-07 12:29:00 +0200

    exercice6JS

---
+++ script.js
@@ -0,0 +1,6 @@
+function clic(){
+var firstNumber = document.getElementById("firstNumber").value;
+var secondNumber = document.getElementById("secondNumber").value;
+var calcule = (firstNumber) % (secondNumber);
+alert(calcule);
+}
